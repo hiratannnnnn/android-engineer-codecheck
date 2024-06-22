5abf56e9035252eb0856d8879391a17919c3a644
hiratannnnnn <tomoya.hirata@wing.ocn.ne.jp>
2024-06-22 18:34:19 +0900

    Update RepositoryFragment.kt

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryFragment.kt
@@ -12,11 +12,11 @@ import coil.load
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.code_check.databinding.FragmentTwoBinding
 
-class TwoFragment : Fragment(R.layout.fragment_two) {
+class RepositoryFragment : Fragment(R.layout.fragment_two) {
 
-    private val args: TwoFragmentArgs by navArgs()
+    private val args: RepositoryFragmentArgs by navArgs()
 
-    private var binding: FragmentTwoBinding? = null
+    private var binding: FragmentRepositoryBinding? = null
     private val _binding get() = binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,7 +24,7 @@ class TwoFragment : Fragment(R.layout.fragment_two) {
 
         Log.d("検索した日時", lastSearchDate.toString())
 
-        binding = FragmentTwoBinding.bind(view)
+        binding = FragmentRepositoryBinding.bind(view)
 
         var item = args.item
 
